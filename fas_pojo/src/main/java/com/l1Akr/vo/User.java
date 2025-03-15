3882cd17ee665b7cf76913858c1701a6d4b0143b
CPerdst <2539439359@qq.com>
2025-03-16 02:26:14 +0800

    new: 实现Login接口，实现Jwt工具类

---
+++ fas_pojo/src/main/java/com/l1Akr/vo/User.java
@@ -0,0 +1,10 @@
+package com.l1Akr.vo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    String username;
+    String password;
+    Boolean sex;
+}
---
+++ fas_server/src/main/java/com/l1Akr/interceptors/JwtInterceptor.java
@@ -0,0 +1,9 @@
+package com.l1Akr.interceptors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtInterceptor {
+
+}
