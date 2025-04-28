c44a3cc28af47de503433664f0cd41fcf9c2c790
CPerdst <2539439359@qq.com>
2025-04-28 17:45:12 +0800

    update：更改单样本大小上限20MB，总样本大小不能超过50MB

--- fas_server/src/main/java/com/l1Akr/config/GlobalExceptionAdvice.java
+++ fas_server/src/main/java/com/l1Akr/config/GlobalExceptionAdvice.java
@@ -7,6 +7,8 @@ import com.l1Akr.common.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestControllerAdvice
 @Slf4j
@@ -28,5 +30,11 @@ public class GlobalExceptionAdvice {
     public Result<String> doException(Exception e) {
         log.warn("doException has occurred: {} {}", e.getClass().getSimpleName(), e.getMessage());
         return Result.error("internal error occurred");
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleSizeExceeded(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("error", "文件大小超过限制");
+        return "redirect:/upload";
     }
 }
